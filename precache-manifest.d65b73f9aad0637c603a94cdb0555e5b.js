1e278e62da85662ecf055f4cebd987e2aa68dc4e
Utkarsh Bansal <utkarshbansal210@gmail.com>
2021-04-01 10:06:49 +0530

    v3.0

---
+++ precache-manifest.d65b73f9aad0637c603a94cdb0555e5b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66493ad2fcf518732ebd4ec720bde122",
+    "url": "/index.html"
+  },
+  {
+    "revision": "283a2e4afcca8b37560a",
+    "url": "/static/css/2.a971abb3.chunk.css"
+  },
+  {
+    "revision": "ac8b918a015bd072408d",
+    "url": "/static/css/main.ea0e61fd.chunk.css"
+  },
+  {
+    "revision": "283a2e4afcca8b37560a",
+    "url": "/static/js/2.29d7e83e.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.29d7e83e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac8b918a015bd072408d",
+    "url": "/static/js/main.25d93956.chunk.js"
+  },
+  {
+    "revision": "2e7e6a13be09944904b1",
+    "url": "/static/js/runtime-main.906d5e0f.js"
+  },
+  {
+    "revision": "7959345f93883bb391d5ea2666535e24",
+    "url": "/static/media/Resume.7959345f.pdf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
