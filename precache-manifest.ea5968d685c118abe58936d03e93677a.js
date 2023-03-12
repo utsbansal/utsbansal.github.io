0093e6953e16aa6faa71b81892d00a9eba4af656
utsbansal <utsbansal@users.noreply.github.com>
2023-03-12 01:26:27 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.ea5968d685c118abe58936d03e93677a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527d078fa88fb57ffc14965a1480fed4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fa420283bc37ef729d5",
+    "url": "/static/css/2.a971abb3.chunk.css"
+  },
+  {
+    "revision": "8609dc2b312075edce5d",
+    "url": "/static/css/main.13bc1d4f.chunk.css"
+  },
+  {
+    "revision": "8fa420283bc37ef729d5",
+    "url": "/static/js/2.85084ce3.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.85084ce3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8609dc2b312075edce5d",
+    "url": "/static/js/main.aa4aae0b.chunk.js"
+  },
+  {
+    "revision": "2e7e6a13be09944904b1",
+    "url": "/static/js/runtime-main.906d5e0f.js"
+  },
+  {
+    "revision": "4e468e0dc42c23c1240ed811a96a8037",
+    "url": "/static/media/Resume.4e468e0d.pdf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
